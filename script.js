58f9ee86580d817b02a32fc98c25b2bc1de5d5d2
Kristian Makhul <kristianmakhul@gmail.com>
2021-10-03 15:48:36 +0300

    release

---
+++ script.js
@@ -0,0 +1,25 @@
+function openTabs(evt, tabsName) {
+     var i, tabcontent, tablinks;
+     tabcontent = document.getElementsByClassName("tabcontent");
+     for (i = 0; i < tabcontent.length; i++) {
+       tabcontent[i].style.display = "none";
+     }
+     tablinks = document.getElementsByClassName("tablinks");
+     for (i = 0; i < tablinks.length; i++) {
+       tablinks[i].classList.remove("active");
+     }
+     document.getElementById(tabsName).style.display = "block";
+     document.getElementById(tabsName + "-tab").classList.add("active");
+     
+   }
+   
+
+   document.getElementById("Activity-tab").click();
+   
+   function sidebar_open() {
+    document.getElementById("aside").style.display = "block";
+  }
+  
+  function sidebar_close() {
+    document.getElementById("aside").style.display = "none";
+  }
