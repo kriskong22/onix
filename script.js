1aa14765f8f63235f2bfae700aea4a29693a7990
Kristian Makhul <kristianmakhul@gmail.com>
2021-10-22 17:13:30 +0300

    2 lesson

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ function openTabs(evt, tabsName) {
   function sidebar_close() {
     document.getElementById("aside").style.display = "none";
   }
+
+  let number = document.querySelector('.number');
+  let arrImg = document.querySelectorAll('.message4 img');
+  let arrayElement = [];
+  for (let i = 0; i < arrImg.length; i++) {
+      arrayElement.push(arrImg[i]);
+      arrImg[i].addEventListener('click', function(e) {
+          number.innerHTML = arrayElement.indexOf(e.target);
+      });
+  }
+
+  let openTasksNumber = document.getElementById("open_tasks_number");
+  openTasksNumber.onclick = function () {
+    let openTasksNumber = document.getElementById("open_tasks_number");
+    if (Number(openTasksNumber.innerHTML) > 0) {
+      if (confirm("Are you sure you want to change the number of tasks?")) {
+        document.getElementById("completed_tasks_number");
+        let completedTasksNumber = document.getElementById("completed_tasks_number");
+        completedTasksNumber.innerHTML = Number(completedTasksNumber.innerHTML) + 1;
+        openTasksNumber.innerHTML = Number(openTasksNumber.innerHTML) - 1;
+      }
+    }
+    else{
+      alert("Number less than 0")
+    }
+  };
+  
